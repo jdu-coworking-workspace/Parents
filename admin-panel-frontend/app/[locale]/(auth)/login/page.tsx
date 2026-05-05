5ef44e7f8cbfe33f3fba1957bdc172ba7a8a3a4c
Asomiddin <asomiddin320@gmail.com>
2026-05-05 14:58:12 +0500

    mini bug show pass

--- admin-panel-frontend/app/[locale]/(auth)/login/page.tsx
+++ admin-panel-frontend/app/[locale]/(auth)/login/page.tsx
@@ -212,7 +212,7 @@ export default function LoginForm() {
                             type="button"
                             onClick={() => setShowPassword(!showPassword)}
                             className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
-                            aria-label={showPassword ? t("hidePassword") : t("showPassword")}
+                            
                           >
                             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
                           </button>
