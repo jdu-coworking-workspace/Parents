0146fc463af08845412fa0dc57bb132ed8a3e8d2
Ayyubbek <224928a@jdu.uz>
2026-05-07 18:24:09 +0500

    654 settings full page UI (#656)
    * settings panel added
    
    * Clean up SettingsLayout by removing unused imports
    
    Removed unused color scheme imports and variables.

--- mobile-students/app/(tabs)/(settings)/_layout.tsx
+++ mobile-students/app/(tabs)/(settings)/_layout.tsx
@@ -1,9 +1,27 @@
 import { Stack } from 'expo-router';
 
 export default function SettingsLayout() {
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          title: 'Sozlamalar',
+          headerStyle: {
+            backgroundColor: '#3B82F6',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 18,
+            color: '#fff',
+          },
+          headerShadowVisible: false,
+          headerTitleAlign: 'center',
+        }}
+      />
     </Stack>
   );
 }
