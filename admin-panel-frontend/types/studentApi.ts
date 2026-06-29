ebdafd11134aa3584dadfd966ab683ca4a95b992
Abdufattoh <72434027+Detor4@users.noreply.github.com>
2026-06-29 10:15:56 +0500

    students dagi qong'iroq fix qilindi (#730)
    * students dagi qong'iroq fix qilindi
    
    * fix: view_at dagi muammo hal qilindi

--- admin-panel-frontend/types/studentApi.ts
+++ admin-panel-frontend/types/studentApi.ts
@@ -1,8 +1,17 @@
 import pagination from "./pagination";
 import Student from "./student";
 
+export interface MessageStudent extends Student {
+  viewed_at?: string | null;
+}
+
 type StudentApi = {
   students: Student[];
+  pagination: pagination;
+};
+
+export type MessageStudentApi = {
+  students: MessageStudent[];
   pagination: pagination;
 };
 
